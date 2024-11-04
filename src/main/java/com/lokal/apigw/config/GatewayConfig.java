1eeccb11ccfd99231609c9e485ddcb00108e92c9
Onur <onrdy96@gmail.com>
2024-11-04 13:09:35 +0300

    fix config

--- src/main/java/com/lokal/apigw/config/GatewayConfig.java
+++ src/main/java/com/lokal/apigw/config/GatewayConfig.java
@@ -18,11 +18,22 @@ public class GatewayConfig {
         return builder.routes()
                 .route("routems", r -> r.path("/api/routes/**")
                         .filters(f -> f.filter(filter))
-                        .uri("http://localhost:8080"))
+                        .uri("http://localhost:8180"))
 
                 .route("authms", r -> r.path("/api/user/**")
                         .filters(f -> f.filter(filter))
                         .uri("http://localhost:8195"))
+
+                .route("explorems", r -> r.path("/api/explore/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://localhost:8085"))
+
+                .route("countryms", r -> r.path("/api/country/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://localhost:8175"))
                 .build();
+
+
+
     }
 }
